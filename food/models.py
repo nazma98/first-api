3e67d9a1c4d8cc8d3e226d24f087b5eab366f509
nazma98 <nazmasarker1311@gmail.com>
2023-04-11 11:21:39 +0600

    Add drinks api files

---
+++ food/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Food(models.Model):
+    name = models.CharField(max_length= 200)
+    rating = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
